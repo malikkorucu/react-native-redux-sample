b675a92281386592eb97bc06b87be1028a540ed6
= <=>
2021-01-23 14:05:30 +0300

    first commit

---
+++ src/redux/index.js
@@ -0,0 +1,27 @@
+// ACTIONS
+
+export const userLogin = () => {
+    return async(dispatch) => {
+        try {
+            const res = await axios.post('URL' , )
+        }catch{
+
+        }
+    }
+}
+
+
+
+
+
+
+//REDUCERS
+
+
+
+
+
+
+
+
+//
